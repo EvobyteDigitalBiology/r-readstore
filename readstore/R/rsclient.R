9936de1acf0855f4a31ceee326c2d5a2498e22fd
JonathanAlles <Jonathan.Alles@evo-byte.com>
2024-11-04 22:40:28 +0100

    Doc: Add in docstrings

--- readstore/R/rsclient.R
+++ readstore/R/rsclient.R
@@ -10,6 +10,13 @@ FQ_ATTACHMENT_ENDPOINT = "fq_attachment/token/"
 PROJECT_ENDPOINT = "project/token/"
 PROJECT_ATTACHMENT_ENDPOINT = "project_attachment/token/"
 
+
+#' test_server_connection
+#'
+#' Check if the server at the endpoint is reachable
+#' 
+#' @param endpoint_url Endpoint to check 
+#' @return TURE if the server is reachable, FALSE otherwise
 test_server_connection <- function(endpoint_url) {
     
     # Create a list of headers
@@ -33,11 +40,19 @@ test_server_connection <- function(endpoint_url) {
     }
 }
 
-auth_user_token <- function(endpoint, username, token) {
+#' auth_user_token
+#'
+#' Check if the user token is valid
+#' 
+#' @param username username
+#' @param token token to authenticate
+#' @param endpoint_url Endpoint to check
+#' @return TRUE if the server token and user is valid, FALSE otherwise
+auth_user_token <- function(username, token, endpoint_url) {
 
     tryCatch({
     # Construct the authentication endpoint URL
-    auth_endpoint <- file.path(endpoint, USER_AUTH_TOKEN_ENDPOINT, fsep = "")
+    auth_endpoint <- file.path(endpoint_url, USER_AUTH_TOKEN_ENDPOINT, fsep = "")
     
     # Prepare the payload
     payload <- list(username = username, token = token)
@@ -57,7 +72,14 @@ auth_user_token <- function(endpoint, username, token) {
   })
 }
 
-
+#' get_rs_client
+#'
+#' Get a rs client object (client template)
+#' 
+#' @param username username
+#' @param token token to authenticate
+#' @param endpoint_url Endpoint to check
+#' @return list with username, token and endpoint_url defining the ReadStore client
 get_rs_client <- function(username, token, endpoint_url) {
 
     endpoint_url = file.path(endpoint_url, REST_API_VERSION, fsep = "")
@@ -65,13 +87,19 @@ get_rs_client <- function(username, token, endpoint_url) {
     if (!test_server_connection(endpoint_url)) {
         stop("Connection Error")
     }
-    if (!auth_user_token(endpoint_url, username, token)) {
+    if (!auth_user_token(username, token, endpoint_url)) {
         stop("Authentication Error")
     }
 
     return(list(username = username, token = token, endpoint_url = endpoint_url))
 }
 
+#' upload_fastq_rs
+#'
+#' Upload fastq files to the ReadStore API
+#' 
+#' @param client ReadStore client
+#' @param fastq_files vector of fastq files to upload
 upload_fastq_rs <- function(client, fastq_files) {
 
     fq_upload_endpoint = file.path(client$endpoint_url, FASTQ_UPLOAD_ENDPOINT, fsep = "")
@@ -99,16 +127,14 @@ upload_fastq_rs <- function(client, fastq_files) {
     }
 }
 
-
+#' get_fq_file_rs
+#'
+#' Get a fastq file from the ReadStore API
+#' 
+#' @param client ReadStore client
+#' @param fq_file_id id of the fastq file
+#' @return json object (list) with fastq file
 get_fq_file_rs <- function(client, fq_file_id) {
-    # Get a fastq file from the ReadStore API
-    #
-    # Args:
-    #     client: ReadStore client
-    #     fq_file_id: Fastq file ID
-    #
-    # Returns:
-    #     Fastq file object
 
     fq_file_endpoint = file.path(client$endpoint_url, FQ_FILE_ENDPOINT, fsep = "")
 
@@ -118,14 +144,25 @@ get_fq_file_rs <- function(client, fq_file_id) {
 
     if (!(httr::status_code(res) %in% c(200, 204))) {
         stop("get_fq_file Failed")
+    } else {
+        json = httr::content(res, "parsed")
+        if (length(json) == 0) {
+            return(list())
+        } else if (length(json) == 1) {
+           return(json[[1]])
+        } else {
+            stop("get_fq_file: Multiple fq files found for fq file id")
+        }
     }
-
-    json = httr::content(res, "parsed")[[1]]
-
-    return(json)
 }
 
-
+#' get_fq_file_upload_path_rs
+#'
+#' Get upload path for a fastq file from the ReadStore API
+#' 
+#' @param client ReadStore client
+#' @param fq_file_id id of the fastq file
+#' @return character with the upload path
 get_fq_file_upload_path_rs <- function(client, fq_file_id) {
     # Get the upload path for a fastq file from the ReadStore API
     #
@@ -136,7 +173,7 @@ get_fq_file_upload_path_rs <- function(client, fq_file_id) {
     # Returns:
     #     Upload path
 
-    fq_file = get_fq_file(client, fq_file_id)
+    fq_file = get_fq_file_rs(client, fq_file_id)
 
     if (!("upload_path" %in% names(fq_file))) {
         stop("Upload Path Not Found")
@@ -145,7 +182,14 @@ get_fq_file_upload_path_rs <- function(client, fq_file_id) {
     return(fq_file$upload_path)
 }
 
-
+#' list_fastq_datasets_rs
+#'
+#' Get list of fastq datasets from the ReadStore API
+#' 
+#' @param client ReadStore client
+#' @param project_id Project ID to filter datasets for
+#' @param project_name Project name to filter datasets for
+#' @return (json) list of fastq datasets
 list_fastq_datasets_rs <- function(client, project_id = NULL, project_name = NULL) {
 
     fq_dataset_endpoint = file.path(client$endpoint_url, FQ_DATASET_ENDPOINT, fsep = "")
@@ -170,7 +214,15 @@ list_fastq_datasets_rs <- function(client, project_id = NULL, project_name = NUL
     return(json)
 }
 
-
+#' get_fastq_dataset_rs
+#'
+#' Get fastq dataset from the ReadStore API
+#' ID or Name must be provided
+#' 
+#' @param client ReadStore client
+#' @param dataset_id Dataset ID to return
+#' @param dataset_name Dataset name to return
+#' @return json object (list) with fastq dataset
 get_fastq_dataset_rs <- function(client, dataset_id = NULL, dataset_name = NULL) {
 
     fq_dataset_endpoint = file.path(client$endpoint_url, FQ_DATASET_ENDPOINT, fsep = "")
@@ -204,7 +256,12 @@ get_fastq_dataset_rs <- function(client, dataset_id = NULL, dataset_name = NULL)
     }
 }
 
-
+#' list_projects_rs
+#'
+#' Get list of projects from the ReadStore API
+#' 
+#' @param client ReadStore client
+#' @return (json) list of projects (list objects)
 list_projects_rs <- function(client) {
 
     project_endpoint = file.path(client$endpoint_url, PROJECT_ENDPOINT, fsep = "")
@@ -222,7 +279,15 @@ list_projects_rs <- function(client) {
     return(json)
 }
 
-
+#' get_project_rs
+#'
+#' Get project from the ReadStore API
+#' ID or Name must be provided
+#' 
+#' @param client ReadStore client
+#' @param project_id Project ID to return
+#' @param project_name Project name to return
+#' @return json object (list) with project
 get_project_rs <- function(client, project_id = NULL, project_name = NULL) {
 
     project_endpoint = file.path(client$endpoint_url, PROJECT_ENDPOINT, fsep = "")
@@ -256,6 +321,18 @@ get_project_rs <- function(client, project_id = NULL, project_name = NULL) {
     }
 }
 
+
+#' download_project_attachment_rs
+#'
+#' Download project attachment from the ReadStore API
+#' Write the attachment to a file
+#' ID or Name must be provided
+#' 
+#' @param client ReadStore client
+#' @param attachment_name Attachment name to download
+#' @param outpath Path to write the attachment
+#' @param project_id Project ID to return
+#' @param project_name Project name to return
 download_project_attachment_rs <- function(client,
                                         attachment_name,
                                         outpath,
@@ -298,7 +375,17 @@ download_project_attachment_rs <- function(client,
 
 }
 
-
+#' download_fq_dataset_attachment_rs
+#'
+#' Download dataset attachment from the ReadStore API
+#' Write the attachment to a file
+#' ID or Name must be provided
+#' 
+#' @param client ReadStore client
+#' @param attachment_name Attachment name to download
+#' @param outpath Path to write the attachment
+#' @param dataset_id Project ID to return
+#' @param dataset_name Project name to return
 download_fq_dataset_attachment_rs <- function(client,
                                         attachment_name,
                                         outpath,
